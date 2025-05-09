bdc82461cd684cb042fe34d80ca1e5d7a2457343
Oliver K <kwizeramugishaolivier0@gmail.com>
2025-05-09 12:39:02 +0200

    Base url bug fixes

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,6 +4,19 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  app: {
+    baseURL: process.env.BASE_URL || "/",
+    head: {
+      title: "Sortify",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+
+        { name: "format-detection", content: "telephone=no" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
   modules: ["@nuxt/icon"],
   vite: {
     plugins: [tailwindcss()],
